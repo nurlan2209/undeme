d23bd2735fb0df563c879a579a8956276544621b
111KartoFan111 <zarkynismagulov@gmail.com>
2026-03-01 15:46:48 +0500

    fix apiconf

--- undeme/lib/core/config/app_config.dart
+++ undeme/lib/core/config/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://93.170.72.89/api',
+    defaultValue: 'http://93.170.72.89:8080/api',
   );
 
   static const int sosCountdownSeconds = 4;
